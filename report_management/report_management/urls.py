d31e39257522ab936a1c12fd3e85548de91479f7
jawaher-alqotym <jwahralqtym@gmail.com>
2022-08-25 11:58:37 +0300

    new commit

--- report_management/report_management/urls.py
+++ report_management/report_management/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('adminpanal/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('users_management_app/', include('users_management_app.urls')),
     path('reports_management_app/', include('reports_management_app.urls')),
