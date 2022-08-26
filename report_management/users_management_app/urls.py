18a447c817da82c48cd7c98de6b51caa6c89ea3e
jawaher-alqotym <jwahralqtym@gmail.com>
2022-08-26 06:12:38 +0300

    new user/groups related functions in views

--- report_management/users_management_app/urls.py
+++ report_management/users_management_app/urls.py
@@ -6,7 +6,10 @@ from . import views
 app_name = "users_management_app"
 
 urlpatterns = [
+               path('register/', views.register, name='register'),
                path('login/', views.login, name='login'),
                path('add_user_to_group/<str:group>/<str:username>/', views.add_user_to_group, name='add_user_to_group'),
                path('remove_user_from_group/<str:group>/<str:username>/', views.remove_user_from_group, name='remove_user_from_group'),
+               path('get_user_info/<str:user_id>/', views.get_user_info, name='get_user_info'),
+               path('get_all_group_member/<str:group>/', views.get_all_group_member, name='get_all_group_member'),
            ]
--- report_management/users_management_app/views.py
+++ report_management/users_management_app/views.py
@@ -10,8 +10,22 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAdminUser
 from rest_framework_simplejwt.tokens import AccessToken
+from rest_framework.pagination import PageNumberPagination
 from django.contrib.auth.models import User
 from .serializers import *
+
+@api_view(["POST"])
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAdminUser])
+def register(request: Request):
+  try:
+    User_serializer = UserSerializer(data=request.data)
+    User_serializer.is_valid(raise_exception=True)
+    User_serializer.save()
+    return Response({"msg": "New user hase been created"})
+
+  except Exception as e:
+        return Response({"msg": f"Couldn't create a user, {e}"}, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
 def login(request: Request):
@@ -58,6 +72,36 @@ def remove_user_from_group(request: Request, group: str, username: str):
      return Response('success', status.HTTP_200_OK)
     except Exception as e:
         return Response(f'{e}', status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAdminUser])
+def get_user_info(request: Request, user_id: str):
+        try:
+          user = User.objects.get(id=user_id)
+          dataResponse = {
+                "user":UserSerializer(instance=user).data
+            }
+          return Response(dataResponse, status=status.HTTP_200_OK)
+
+        except Exception as e:
+          return Response({"msg": f"{e}"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAdminUser])
+def get_all_group_member(request: Request, group: str):
+    try:
+        paginator = PageNumberPagination()
+        users = User.objects.filter(groups__name= group).order_by('-id')
+        result_page = paginator.paginate_queryset(users, request)
+        serializer = UserSerializer(result_page, many=True)
+        return paginator.get_paginated_response(serializer.data)
+
+    except Exception as e:
+        return Response({"msg": f"{e}"}, status=status.HTTP_400_BAD_REQUEST)
+
 
 
 
